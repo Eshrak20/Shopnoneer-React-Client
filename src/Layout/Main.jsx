f59c5d624bfb6daf5e21a669ebc3a7b07102aa0f
Eshrak20 <uemran2018@gmail.com>
2024-11-06 21:28:03 +0600

    remove footer from detail

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
   const location = useLocation();
 
   const noHeaderFooter =
-    location.pathname.includes("login") || location.pathname.includes("signup");
+    location.pathname.includes("login") || location.pathname.includes("signup") || location.pathname.includes("detail");
   return (
     <div>
       {noHeaderFooter || <Navbar></Navbar>}
