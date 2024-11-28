9dfe784c397cf8c2d44432851522dc8340bb4d53
Eshrak20 <uemran2018@gmail.com>
2024-11-28 22:22:41 +0600

    28-11-24

--- src/Pages/DetailsProperty/DetailsPropMainCard/DetailsPropMainCard.jsx
+++ src/Pages/DetailsProperty/DetailsPropMainCard/DetailsPropMainCard.jsx
@@ -87,72 +87,78 @@ const DetailsPropMainCard = ({ property }) => {
         />
       )}
 
-<div className="pb-4 mb-7">
-  <section className="grid grid-cols-1 lg:grid-cols-3 gap-2">
-    <div className="lg:col-span-2 space-y-6 bg-white rounded-lg relative">
-      {/* Bookmark button */}
-      <div className="absolute top-10 right-4 lg:left-96 z-10">
-        <button className="text-teal-500 hover:text-teal-700 focus:outline-none">
-          <FontAwesomeIcon icon={faBookmark} className="text-lg lg:text-2xl" />
-        </button>
+      <div className="pb-4 mb-7">
+        <section className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+          <div className="lg:col-span-2 space-y-6 bg-white rounded-lg relative">
+            {/* Bookmark button */}
+            <div className="absolute top-7 right-4 lg:left-96 z-10">
+              <button className="bg-teal-500 px-4 py-2 rounded group hover:bg-white">
+                <FontAwesomeIcon
+                  icon={faBookmark}
+                  className="text-lg text-gray-100 group-hover:text-teal-600 lg:text-2xl"
+                />
+              </button>
+            </div>
+
+            <h1 className="text-5xl font-bold text-gray-900 mb-4">
+              {property.title}
+            </h1>
+            <h4 className="text-xl text-teal-600 flex flex-col mb-4">
+              <span className="flex items-center">
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
+                {capitalizeFirstChar(property.division)},{" "}
+                {capitalizeFirstChar(property.district)},{" "}
+                {capitalizeFirstChar(property.upazila)},{" "}
+                {capitalizeFirstChar(property.housing)},{" "}
+              </span>
+            </h4>
+
+            <h3 className="text-3xl font-semibold text-gray-900 mt-4 mb-4">
+              Price: ৳{" "}
+              {property.total_price
+                ? property.total_price.toLocaleString()
+                : "Upcoming"}
+            </h3>
+
+            <div className="flex gap-4 my-4 items-center text-gray-600 text-lg">
+              <FontAwesomeIcon icon={faBed} />
+              <span>{property.no_of_beds} Beds</span>
+              <FontAwesomeIcon icon={faBath} />
+              <span>{property.no_of_baths} Baths</span>
+              <FontAwesomeIcon icon={faCity} />
+              <span>{property.no_of_balcony} Balcony</span>
+              <FontAwesomeIcon icon={faHome} />
+              <span>{property.rate_per_sqft} sqft</span>
+            </div>
+
+            <div className="my-4">
+              <h4 className="text-xl mb-2 font-semibold text-gray-900">
+                Overview
+              </h4>
+              <p className="text-gray-600 text-lg">{property.description}</p>
+            </div>
+
+            <div className="my-4">
+              <h4 className="text-lg font-semibold text-gray-900">
+                Additional Information
+              </h4>
+              <p className="text-gray-600 text-lg">
+                Plot: {property.plot}, Road: {property.road}
+              </p>
+              <p className="text-gray-600 text-lg">
+                Block: {property.block}, Plot Size: {property.plot_size} sqft
+              </p>
+              <p className="text-gray-600 text-lg">
+                Floor Area: {property.floor_area} sqft, Floor Number:{" "}
+                {property.floor_no}
+              </p>
+            </div>
+          </div>
+          <div className="-mt-16">
+            <GoogleMap></GoogleMap>
+          </div>
+        </section>
       </div>
-
-      <h1 className="text-5xl font-bold text-gray-900 mb-4">{property.title}</h1>
-      <h4 className="text-xl text-teal-600 flex flex-col mb-4">
-        <span className="flex items-center">
-          <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
-          {capitalizeFirstChar(property.division)},{" "}
-          {capitalizeFirstChar(property.district)},{" "}
-          {capitalizeFirstChar(property.upazila)},{" "}
-          {capitalizeFirstChar(property.housing)},{" "}
-        </span>
-      </h4>
-
-      <h3 className="text-3xl font-semibold text-gray-900 mt-4 mb-4">
-        Price: ৳{" "}
-        {property.total_price
-          ? property.total_price.toLocaleString()
-          : "Upcoming"}
-      </h3>
-
-      <div className="flex gap-4 my-4 items-center text-gray-600 text-lg">
-        <FontAwesomeIcon icon={faBed} />
-        <span>{property.no_of_beds} Beds</span>
-        <FontAwesomeIcon icon={faBath} />
-        <span>{property.no_of_baths} Baths</span>
-        <FontAwesomeIcon icon={faCity} />
-        <span>{property.no_of_balcony} Balcony</span>
-        <FontAwesomeIcon icon={faHome} />
-        <span>{property.rate_per_sqft} sqft</span>
-      </div>
-
-      <div className="my-4">
-        <h4 className="text-xl mb-2 font-semibold text-gray-900">Overview</h4>
-        <p className="text-gray-600 text-lg">{property.description}</p>
-      </div>
-
-      <div className="my-4">
-        <h4 className="text-lg font-semibold text-gray-900">
-          Additional Information
-        </h4>
-        <p className="text-gray-600 text-lg">
-          Plot: {property.plot}, Road: {property.road}
-        </p>
-        <p className="text-gray-600 text-lg">
-          Block: {property.block}, Plot Size: {property.plot_size} sqft
-        </p>
-        <p className="text-gray-600 text-lg">
-          Floor Area: {property.floor_area} sqft, Floor Number:{" "}
-          {property.floor_no}
-        </p>
-      </div>
-    </div>
-    <div className="-mt-16">
-      <GoogleMap></GoogleMap>
-    </div>
-  </section>
-</div>
-
     </>
   );
 };
